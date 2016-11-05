d597d260d0fe65c5d006dafcab9106b6ee43a1c8
grover <grover.plaza@gmail.com>
2016-11-05 16:56:21 -0400

    try another time

---
+++ app.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var favicon = require('serve-favicon');
+var app = express();
+app.use('/public', express.static('public'));
+app.use(favicon(__dirname + '/public/favicon.ico'));
+app.set('view engine', 'pug');
+app.get('/', function (req, res) {
+  res.render('index', { title: 'kuntur', message: 'testeando'});
+});
+
+app.listen(80, function () {
+  console.log('corriendo en el puerto 80 como debe ser!');
+});
