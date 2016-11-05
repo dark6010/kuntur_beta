3c0ca839c025c06ba75d1d0e003b1e8489726feb
grover <grover.plaza@gmail.com>
2016-11-05 18:11:37 -0400

    ff

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var favicon = require('serve-favicon');
 var app = express();
-app.use('/', express.static('public'));
+app.use('/', express.static(__dirname + 'public'));
 app.use(favicon(__dirname + '/public/favicon.ico'));
 app.set('view engine', 'pug');
 app.get('/', function (req, res) {
