47603bee23b5d50d6485295977965577eba838c5
grover <grover.plaza@gmail.com>
2016-11-05 18:18:19 -0400

    ultra last

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 var express = require('express');
-var favicon = require('serve-favicon');
+//var favicon = require('serve-favicon');
 var app = express();
-app.use('/', express.static(__dirname + 'public'));
-app.use(favicon(__dirname + '/public/favicon.ico'));
+app.use('/', express.static(__dirname + '/public'));
+//app.use(favicon(__dirname + '/public/favicon.ico'));
 app.set('view engine', 'pug');
 app.get('/', function (req, res) {
   res.render('index', { title: 'kuntur', message: 'testeando'});
